5f968dc29d4cef28e6e4c031706818bbc37d9706
Sujana Kola <76068884+Sujanakola@users.noreply.github.com>
2023-04-02 12:13:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 (function () {
 
-    var names = ["Sujana", "Suma", "Jin", "Jacob", "Zendaya", "Taylor", "Pine", "Apple", "Lauryn", "ken"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
